e2ddcde306de250e49718267717ac4ac442287d2
Ricky <ricky.holtz@berkeley.edu>
2015-12-09 16:15:40 -0800

    fighting with grunt

---
+++ _site/Gruntfile.js
@@ -0,0 +1,97 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Show elapsed time after tasks run to visualize performance
+    require('time-grunt')(grunt);
+    // Load all Grunt tasks that are listed in package.json automagically
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // shell commands for use in Grunt tasks
+        shell: {
+            jekyllBuild: {
+                command: 'jekyll build'
+            },
+            jekyllServe: {
+                command: 'jekyll serve'
+            }
+        },
+
+        watch: {
+          sass: {
+            files: ['_sass/**/*.{scss,sass}'],
+            tasks: ['sass']
+          }
+        },
+
+        // sass (libsass) config
+        sass: {
+            options: {
+                sourceMap: true,
+                relativeAssets: false,
+                outputStyle: 'expanded',
+                sassDir: '_sass',
+                cssDir: '_site/css'
+            },
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: '_sass/',
+                    src: ['**/*.{scss,sass}'],
+                    dest: '_site/css',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        //autoprefixer config
+        autoprefixer: {
+          dist: {
+            files: {
+              'build/main.css': 'main.css'
+            }
+          }
+        },
+
+        watch: {
+          styles: {
+            files: ['main.css'],
+            tasks: ['autoprefixer']
+          }
+        },
+
+        // run tasks in parallel
+        concurrent: {
+            serve: [
+                'sass',
+                'watch',
+                'shell:jekyllServe'
+            ],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+
+    });
+
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Register the grunt serve task
+    grunt.registerTask('serve', [
+        'shell:jekyllServe'
+    ]);
+
+
+    // Register the grunt build task
+    grunt.registerTask('build', [
+        'shell:jekyllBuild'
+    ]);
+
+    // Register build as the default task fallback
+    grunt.registerTask('default', 'build');
+
+};
--- _site/README.md
+++ _site/README.md
@@ -3,7 +3,4 @@ rickydesign.me
 
 Using
 <ul>
-<a href="http://alvarotrigo.com/fullPage/">FullScreen.js</a> by <a href="https://github.com/alvarotrigo">Alvaro Trigo</a>.
-<a href="http://unslider.com/">unslider.js</a> by <a href="https://github.com/idiot">Idiot</a>.
 </ul>
-
