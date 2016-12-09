38509e49c4433f562ee3f08561157b6281dc40e6
Ricky <ricky.holtz@berkeley.edu>
2016-12-08 18:05:27 -0800

    idk, man

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,17 @@
 'use strict'
 
 var gulp          = require('gulp'),
-    gutil         = require('gulp-util'),
     sass          = require('gulp-sass'),
     imageop       = require('gulp-image-optimization'),
     cp            = require('child_process'),
     plumber       = require('gulp-plumber'),
     autoprefixer  = require('gulp-autoprefixer'),
-    notify        = require('gulp-notify'),
     browserSync   = require('browser-sync'),
     cmq           = require('gulp-combine-mq')
 
-
 // Uses Sass compiler to process styles, adds vendor prefixes, minifies,
 // and then outputs file to appropriate location(s)
-gulp.task('sass', function(done) {
+gulp.task('sass', function (done) {
   return gulp.src('assets/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
@@ -25,38 +22,38 @@ gulp.task('sass', function(done) {
     .pipe(browserSync.stream())
 })
 
-gulp.task('images', function(cb){
+gulp.task('images', function (cb) {
   gulp.src(['_img/**/*.png', '_img/**/*.jpg']).pipe(imageop({
     optimizationLevel: 5,
     progressive: true,
     interlaced: true
-  })).pipe(gulp.dest('assets/img').on('end', cb).on('error',cb))
+  })).pipe(gulp.dest('assets/img').on('end', cb).on('error', cb))
 })
 
 // Run Jekyll Build Asynchronously
 gulp.task('build:jekyll', function (done) {
-    browserSync.notify('Building Jekyll')
-    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
-    .on('close', done)
+  browserSync.notify('Building Jekyll')
+  return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+  .on('close', done)
 })
 
-gulp.task('jekyll-rebuild', ['build:jekyll'], function(){
+gulp.task('jekyll-rebuild', ['build:jekyll'], function () {
   browserSync.reload()
 })
 
-gulp.task('browser:sync', ['build:jekyll'], function(){
+gulp.task('browser:sync', ['build:jekyll'], function () {
   browserSync({
-    server:{
+    server: {
       baseDir: '_site'
     },
     host: 'localhost'
   })
 })
 
-gulp.task('watch', function(){
-  //watch .scss files
+gulp.task('watch', function () {
+  // watch .scss files
   gulp.watch('assets/scss/**/*.scss', ['sass'])
-  //watch html files
+  // watch html files
   gulp.watch(['index.html',
               '_includes/*.html',
               '_layouts/*.html',
@@ -65,6 +62,6 @@ gulp.task('watch', function(){
               ['jekyll-rebuild'])
 })
 
-gulp.task('default', function(){
+gulp.task('default', function () {
   gulp.start('sass', 'browser:sync', 'watch')
 })
